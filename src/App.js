0f0801929f12eac6542a48de3a0abc4dcd9de941
dsantucho <soledadsantucho@hotmail.com>
2023-05-29 10:41:25 -0300

    compila vacio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='header-brand'></div>
+      <div className='header-search'></div>
+      <div className='header-nav'>
+        <div className='header-nav-item'>
+          <span className='text-3xl font-bold'>Hello Guest</span>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//Styling
 import './index.css';
+
+//components
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
