c4b1ab3788d4c23d39cb174c3467c9d7efa72ec3
dsantucho <soledadsantucho@hotmail.com>
2023-06-02 21:25:33 -0300

    login style

--- src/components/Login.js
+++ src/components/Login.js
@@ -39,29 +39,30 @@ const Login = () => {
         });
     }
   return (
-    <div>
-        <Link to='/' style={{textDecoration: 'none'}}>
-            <div className="flex items-center">
-                <StoreIcon fontSize="large" className=" mx-3 text-orange" />
-                <h2 className="font-bold  mr-3">eShop</h2>
+    <div className='flex justify-center'>
+        <div className='inline-flex w-2/4 flex-col items-center  my-11 border-2 rounded border-orange'>
+            <div className="flex  flex-col items-center justify-center">
+                <Link to='/' style={{textDecoration: 'none'}}>
+                    <StoreIcon  style={{ fontSize: '6rem' }} className=" mx-3 text-orange"  />
+                    <h2 className="font-bold  mr-3 flex justify-center">eShop</h2>
+                </Link>
             </div>
-        </Link>
-        <div>
-            <h2>Sign In</h2>
-        <form>
-            <h3>Email</h3>
-            <input type='text' value={email} onChange={e=>setEmail(e.target.value)}/>
+            <div className='flex flex-col my-3'>
+                <h2 className='text-2xl'>Sign In</h2>
+                <form className='flex flex-col p-1'>
+                    <h3 className='text-lg my-2'>Email</h3>
+                    <input type='text' value={email} onChange={e=>setEmail(e.target.value)} className='p-1'/>
 
-            <h3>Password</h3>
-            <input type='password' value={password} onChange={e=>setPassword(e.target.value)}/>
+                    <h3 className='text-lg my-2'>Password</h3>
+                    <input type='password' value={password} onChange={e=>setPassword(e.target.value)} className='p-1'/>
 
-            <button type='submit' onClick={signIn}>Sign In</button>
-        </form>
+                    <button type='submit' onClick={signIn} className='my-2 bg-orange-light p-1'>Sign In</button>
+                </form>
+            </div>
+            <div>
+                <button onClick={register} className='my-2 bg-greyDarker p-1'>Create your eShop account</button>
+            </div>    
         </div>
-        <div>
-            <button onClick={register}>Create your eShop account</button>
-        </div>
-        
     </div>
   )
 }
